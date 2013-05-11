fc8e69a3cbcb3aa8e5bbb846000b72b8343fe0dd
SFantasy <forever.fantasy27@gmail.com>
2013-05-11 16:05:59 +0800

    Update: heap sort solution.

--- Sort/src/sort/Sort.java
+++ Sort/src/sort/Sort.java
@@ -6,11 +6,11 @@ package sort;
  */
 public class Sort {
     // Test array
-    private static Integer[] array = {3, 2, 10, 5, 1, 9, 4, 3};
+    private static int[] array = {3, 2, 10, 5, 1, 9, 4, 3};
 
     public static void main(String[] args) {
         
-        Integer[] newArray = new Integer[array.length];
+//        int[] newArray = new int[array.length];
         
         System.out.println("The original array is: ");
         for (int i = 0; i < array.length; i++) {
@@ -22,16 +22,16 @@ public class Sort {
         //Sort.selectionSort();
         //Sort.bubbleSort();
         //Sort.quickSort();
-        //Sort.heapSort();
-        System.arraycopy(Sort.mergeSort(array), 0, newArray, 0, array.length);
+        Sort.heapSort();
+        //System.arraycopy(Sort.mergeSort(array), 0, newArray, 0, array.length);
 
-//        for (int j = 0; j < array.length; j++) {
-//            System.out.print(array[j] + " ");
-//        }
-        // Output scope ofr Merge sort
-        for(int k = 0; k < newArray.length; k++) {
-            System.out.print(newArray[k] + " ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
         }
+        // Output scope ofr Merge sort
+//        for(int k = 0; k < newArray.length; k++) {
+//            System.out.print(newArray[k] + " ");
+//        }
         System.out.println();
     }
     /*
@@ -86,41 +86,93 @@ public class Sort {
         int i = p,
                 j = r + 1;
 
-        Comparable x = array[p];
-
+        int x = array[p];
+        
         while (true) {
-            while (array[++i].compareTo((Integer) x) < 0) {
+            while (array[++i] < x) {
                 // empty
             }
-            while (array[--j].compareTo((Integer) x) > 0) {
+            while (array[--j] > x) {
                 // empty
             }
             if (i >= j) {
                 break;
             }
-            Integer temp = array[i];
+            int temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
         array[p] = array[j];
-        array[j] = (Integer)x;
+        array[j] = (int)x;
         return j;
     }
     /*
-     *   Heap Sort
+     *   Heap Sort 
+     *   http://zh.wikipedia.org/wiki/%E5%A0%86%E6%8E%92%E5%BA%8F#java.E8.AF.AD.E8.A8.80
      */
     public static void heapSort() {
+        buildMaxHeapify(array);
+        heap(array);
+    }
+    
+    private static void buildMaxHeapify(int[] data){
+        int startIndex = getParentIndex(data.length - 1);
+        
+        for (int i = startIndex; i >= 0; i--) {
+                maxHeapify(data, data.length, i);
+        }
+    }
+    
+    private static void maxHeapify(int[] data, int heapSize, int index){
+        int left = getLeftChildIndex(index);
+        int right = getRightChildIndex(index);
+
+        int largest = index;
+        if (left < heapSize && data[index] < data[left]) {
+                largest = left;
+        }
+        if (right < heapSize && data[largest] < data[right]) {
+                largest = right;
+        }
+
+        if (largest != index) {
+                int temp = data[index];
+                data[index] = data[largest];
+                data[largest] = temp;
+                maxHeapify(data, heapSize, largest);
+        }
+    }
+    
+    private static void heap(int[] data) {
+        for(int i = data.length - 1; i > 0; i--) {
+            int temp = data[0];
+            data[0] = data[i];
+            data[i] = temp;
+            maxHeapify(data, i, 0);
+        }
+    }
+    
+    private static int getParentIndex(int current) {
+        return (current - 1) >> 1;
+    }
+    
+    private static int getLeftChildIndex(int current) {
+        return (current << 1) + 1;
+    }
+    
+    private static int getRightChildIndex(int current) {
+        return (current << 1) + 2;
     }
     /*
      *   Merge Sort
      */
-    public static Integer[] mergeSort(Integer[] arr) {
+    public static int[] mergeSort(int[] arr) {
         if(arr.length == 1) {
             return arr;
         }
         int mid = arr.length / 2;
-        Integer[] left = new Integer[mid];
-        Integer[] right = new Integer[arr.length - mid];
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
         System.arraycopy(arr, 0, left, 0, left.length);
         System.arraycopy(arr, mid, right, 0, right.length);
         left = mergeSort(left);
@@ -128,8 +180,8 @@ public class Sort {
         return merge(left, right);
     }
     
-    public static Integer[] merge(Integer[] l, Integer[] r) {
-        Integer[] result = new Integer[l.length + r.length];
+    public static int[] merge(int[] l, int[] r) {
+        int[] result = new int[l.length + r.length];
         int i = 0,
                 j = 0,
                 k = 0;
