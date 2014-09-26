170c6387bef5515d17b962710622c9442b726340
Fantasy Shao <forever.fantasy27@gmail.com>
2014-09-26 20:39:47 +0800

    Add CArray class.

--- SortJS/CArray.js
+++ SortJS/CArray.js
@@ -1,29 +1,55 @@
+/**
+ *
+ * CArray
+ *
+ * @credit p145 on Data Structure and Algorithms with JavaScript
+ * @description
+ * @create 2014-09-21
+ * @update 2014-09-26
+ */
+
 var CArray = function (numElements) {
 
   this.dataStore = [];
   this.pos = 0;
   this.numElements = numElements;
 
+  for (var i = 0 ; i < numElements; ++i) {
+    this.dataStore[i] = i;
+  }
+
 };
 
-CArray.prototype.insert = function () {
-
+CArray.prototype.insert = function (el) {
+    this.dataStore[this.pos++] = el;
 };
 
 CArray.prototype.toString = function () {
-
+    var restr = '';
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        restr += this.dataStore[i] + ' ';
+        if (i > 0 & i % 10 == 0) {
+            restr += '\n';
+        }
+    }
 };
 
 CArray.prototype.clear = function () {
-
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        this.dataStore[i] = 0;
+    }
 };
 
 CArray.prototype.setData = function () {
-
+    for (var i = 0; i < this.numElements; ++i) {
+        this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+    }
 };
 
-CArray.prototype.swap = function () {
-  
+CArray.prototype.swap = function (arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 };
 
 module.exports = CArray;
