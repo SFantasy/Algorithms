3fc5ed805f62135764c4cd8a31b41964270adaea
Fantasy Shao <forever.fantasy27@gmail.com>
2014-09-26 20:34:24 +0800

    Add gitignore file.

---
+++ SortJS/BubbleSort.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * BubbleSort
+ *
+ * @description
+ * @author Fantasy <fantasyshao@icloud.com>
+ * @create 2014-09-21
+ * @update 2014-09-21
+ */
---
+++ SortJS/CArray.js
@@ -0,0 +1,29 @@
+var CArray = function (numElements) {
+
+  this.dataStore = [];
+  this.pos = 0;
+  this.numElements = numElements;
+
+};
+
+CArray.prototype.insert = function () {
+
+};
+
+CArray.prototype.toString = function () {
+
+};
+
+CArray.prototype.clear = function () {
+
+};
+
+CArray.prototype.setData = function () {
+
+};
+
+CArray.prototype.swap = function () {
+  
+};
+
+module.exports = CArray;
