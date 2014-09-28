a3516cf512de081120c22d6afbefe22e01ddb25a
Fantasy Shao <forever.fantasy27@gmail.com>
2014-09-28 22:33:18 +0800

    add bubblesort.

--- SortJS/BubbleSort.js
+++ SortJS/BubbleSort.js
@@ -7,3 +7,20 @@
  * @create 2014-09-21
  * @update 2014-09-21
  */
+
+var CArray = require('./CArray');
+var arr = new CArray();
+
+module.exports = function () {
+    var numElements = arr.dataStore.length;
+
+    var temp;
+
+    for (var i = numElements; i >= 2; --i) {
+        for (var j = 0; j < i - 1; ++j) {
+            if (arr.dataStore[j] > arr.dataStore[j + 1]) {
+                arr.swap(arr.dataStore, j , j+1);
+            }
+        }
+    }
+};
